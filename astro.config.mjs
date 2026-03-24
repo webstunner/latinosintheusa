4eb355d9e2173f7f34ce9c98264040ccafc9873b
webstunner <84951413+webstunner@users.noreply.github.com>
2026-03-23 23:45:57 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+
 export default defineConfig({
-  output: 'static'
+  output: 'static',
+  integrations: [tailwind()]
 });
